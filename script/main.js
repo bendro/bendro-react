3f23d13778088b55f69505d065ca4e4f7f3a7a1f
Thomas Heck <t@b128.net>
2015-03-15 16:33:30 +0100

    + react-intl de locale

--- script/main.js
+++ script/main.js
@@ -2,6 +2,28 @@
 var React = require('react')
 var comps = require('./components.js')
 
+require('./locales/de')
+
 module.exports = function renderComments(props, elem) {
+	props.locales = 'de'
+
+	props.formats = {
+		date: {
+			datetime: {
+				year: 'numeric',
+				month: 'short',
+				day: 'numeric',
+				hour: '2-digit',
+				minute: '2-digit',
+			},
+		},
+	}
+
+	props.messages = {
+		anonymous: 'Anonymus',
+		commentHeader: '{author} schrieb {ctime}',
+		commentHeaderEdited: '{author} schrieb {ctime} (bearbeitet {mtime})',
+	}
+
 	return React.render(comps.Comments(props), elem)
 }
