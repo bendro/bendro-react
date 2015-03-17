88ac0bd5d2745fdc95f4cad55e5e77471526078a
Thomas Heck <t@b128.net>
2015-03-17 22:39:06 +0100

    use the new Api class

--- script/comments.js
+++ script/comments.js
@@ -1,7 +1,6 @@
 'use strict'
 var React = require('react')
 var ReactIntl = require('react-intl')
-var request = require('superagent')
 var comps = require('./components.js')
 
 var rd = React.DOM
@@ -11,73 +10,15 @@ module.exports = React.createClass({
 
 	mixins: [ReactIntl.IntlMixin],
 
-	getInitialState: function() {
-		return {
-			comments: [],
-		}
-	},
-
-	componentDidMount: function() {
-		this.loadComments()
-	},
-
-	loadComments: function() {
-		request
-			.get(this.props.url + 'comments?site=' + this.props.site)
-			.end(function(err, res) {
-				if(err) {
-					throw err
-				}
-				var cs = []
-				var cur = null
-
-				res.body.forEach(function(c) {
-					if(c.left === 0) {
-						cs.push(c)
-						cur = c
-						return
-					}
-
-					while(cur.right < c.right) {
-						cur = cur.parent
-					}
-
-					c.parent = cur
-					cur.children = cur.children || []
-					cur.children.push(c)
-					cur = c
-				})
-
-				this.setState({comments: cs})
-			}.bind(this))
-	},
-
-	onSendComment: function(comment) {
-		comment.site = this.props.site
-
-		request
-			.post(this.props.url + 'comment')
-			.send(comment)
-			.end(function(err, res) {
-				if(err) {
-					throw err
-				}
-
-				this.setState({
-					comments: [res.body].concat(this.state.comments),
-				})
-			}.bind(this))
-	},
-
 	render: function() {
 		return rd.div(
 			{},
 			comps.commentForm({
-				onSendComment: this.onSendComment,
+				onSendComment: this.props.onSendComment,
 			}),
 			comps.commentList({
-				comments: this.state.comments,
-				onSendComment: this.onSendComment,
+				comments: this.props.comments,
+				onSendComment: this.props.onSendComment,
 			})
 		)
 	},
--- script/main.js
+++ script/main.js
@@ -1,12 +1,20 @@
 'use strict'
 var React = require('react')
 var comps = require('./components.js')
+var Api = require('./api.js')
 
 require('./locales/de')
 
-module.exports = function renderComments(props, elem) {
-	props.locales = 'de'
+module.exports = function renderComments(site, options, elem) {
+	var conn = new Api(options.url, site)
 
+	var comp
+
+	var props = {
+		comments: [],
+	}
+
+	props.locales = 'de'
 	props.formats = {
 		date: {
 			datetime: {
@@ -18,12 +26,34 @@ module.exports = function renderComments(props, elem) {
 			},
 		},
 	}
-
 	props.messages = {
 		anonymous: 'Anonymus',
 		commentHeader: '{author} schrieb {ctime}',
 		commentHeaderEdited: '{author} schrieb {ctime} (bearbeitet {mtime})',
 	}
 
-	return React.render(comps.comments(props), elem)
+	props.onSendComment = function(comment) {
+		conn.postComment(comment, function(err, commentRes) {
+			if(err) {
+				throw err
+			}
+
+			// TODO: add to proper position
+			comp.setProps({
+				comments: comp.props.comments.concat([commentRes]),
+			})
+		})
+	}
+
+	comp = React.render(comps.comments(props), elem)
+
+	conn.getComments(function(err, res) {
+		if(err) {
+			throw err
+		}
+
+		comp.setProps({comments: res.comments})
+	})
+
+	return comp
 }
